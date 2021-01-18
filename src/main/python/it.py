d6235034d7d4187cbfd2984506b44e05aef0b7c7
Nikolaus Winter <mail@nikolaus-winter.de>
2021-01-18 07:28:15 +0100

    connect to t2 API

--- src/main/python/it.py
+++ src/main/python/it.py
@@ -1,4 +1,20 @@
+# import the standard JSON parser
+import json
+
+# import the REST library
+import requests
+
 print('Starting integration test...')
-print('Started integration test.')
+
+resp = requests.get('http://t2.stackable.tech/api/clusters')
+if resp.status_code != 200:
+    # This means something went wrong.
+    print('ERROR')
+    # raise ApiError('GET /tasks/ {}'.format(resp.status_code))
+for todo_item in resp.json():
+    # print('{} {}'.format(todo_item['id'], todo_item['summary']))
+    print('cluster {} up and running'.format(todo_item['id']))
+
+
 print('SUCCESS')
 print('integration test finished.')
