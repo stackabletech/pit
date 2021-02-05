4328b10e3a4b7d04637f73530515f78d2e64b0c5
Nikolaus Winter <mail@nikolaus-winter.de>
2021-02-05 07:16:24 +0100

    waiting for cluster to be properly shut down

--- src/main/python/terminate.py
+++ src/main/python/terminate.py
@@ -20,7 +20,35 @@ def delete_cluster(id):
     response = requests.delete(f"{t2_base_url}/api/clusters/{id}", headers={ "t2-token": t2_token })
     if(response.status_code != 200):
         print(f"API call to delete cluster returned error code {response.status_code}");
-        return False
-    return True
+        return None
+    return response.json()
 
-delete_cluster(t2_cluster_id)
+def get_cluster(id):
+    response = requests.get(f"{t2_base_url}/api/clusters/{id}", headers={ "t2-token": t2_token })
+    if(response.status_code != 200):
+        print(f"API call to get cluster returned error code {response.status_code}");
+        return None
+    return response.json()
+
+def update_cluster(cluster):
+    updated_cluster = get_cluster(cluster['id'])
+    if(not updated_cluster):
+        return cluster
+    return updated_cluster
+
+cluster = delete_cluster(t2_cluster_id)
+
+if(not cluster):
+    print("Failed to terminate cluster via API.")
+    exit(1)
+
+print(f"Created cluster '{cluster['id']}'. Waiting for cluster to be properly shut down...")
+time.sleep(5)
+
+cluster = update_cluster(cluster)
+while(cluster['status']['state'] != 'TERMINATED'):
+    print(f"Cluster is still in state '{cluster['status']['state']}'', waiting for it to be in state 'TERMINATED'")
+    time.sleep(5)
+    cluster = update_cluster(cluster)
+
+print(f"Cluster '{cluster['id']}' has been terminated.")
